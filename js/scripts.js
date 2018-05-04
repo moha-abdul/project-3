6a27c708d6e95a67f9465c65ba33d0694b25fb7a
Mohammed Abdullahi <mohammed.abdulla0602@gmail.com>
2018-05-04 15:39:41 +0300

    Add ping for numbers divisible by 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
-/*this function takes vowel as an input*/
+/*this function takes a number as an input*/
 
-var pingPong = function(ping) {
-  return false;
+var pingPong = function(myNumber) {
+  var myArray = [];
+  for (var newNumbers = 1; newNumbers <= myNumber; newNumbers += 1) {
+    if (newNumbers % 3 == 0 && newNumbers % 5 != 0) {
+      myArray.push("ping");
+    } else {
+      myArray.push(newNumbers);
+    }
+  }
+  return myArray.toString();
 }
 
 $(document).ready(function(){
-  /*when you click on element button, class btn, id ping the submit function is initiated*/
+  /*when you click on element button, class=btn, id=ping the submit function is initiated*/
   $("form#enterPing").submit(function(event) {
     event.preventDefault();
-    /*ping takes in input*/
-    var ping = parseInt($("input#ping").val());
+    /*myNumber takes in input*/
+    var myNumber = parseInt($("input#myNumber").val());
     /*result is the output of the function pingPong*/
-    var result = pingPong(ping);
+    var result = pingPong(myNumber);
     // this line outputs to html
     $("#result").text(result);
   });
