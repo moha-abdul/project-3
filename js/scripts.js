619f329054bc252e327bc936d1a1aa9d3590990a
Mohammed Abdullahi <mohammed.abdulla0602@gmail.com>
2018-05-04 09:16:42 +0300

    Add basic html structure with links to javascript and css files

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+/*this function takes vowel as an input*/
+var words = function(vowel) {
+  if (vowel 
+  return false;
+}
+
+$(document).ready(function() {
+  /*when you click on element button, class btn, the submit function is initiated*/
+  $("button.btn").submit(function(event) {
+    event.preventDefault();
+    /*vowel takes in input*/
+    var vowel = $("input#inputWord").val();
+    /*result is the output of the function words*/
+    var result = words (vowel) {
+
+    };
+  });
+});
